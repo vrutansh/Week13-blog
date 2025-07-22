43d51b2066c7486ce1e4b7a1add9fd3cdeb0f0ca
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-07-22 20:42:56 +0530

    signin/up done

--- myblog/src/index.ts
+++ myblog/src/index.ts
@@ -5,12 +5,27 @@ import {decode, sign, verify } from 'hono/jwt'
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
-    JWT_SECRET: string
+    JWT_SECRET: string,
+    
   }
 }>()
-app.use('api/v1/blog/*', async (c, next) => {
-  await next()
+
+app.use('/api/v1/blog/*', async (c, next) => {
+	const jwt = c.req.header('Authorization');
+	if (!jwt) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	const token = jwt.split(' ')[1];
+	const payload = await verify(token, c.env.JWT_SECRET);
+	if (!payload) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	c.set('userId', payload.id);
+	await next()
 })
+
 app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl:c.env.DATABASE_URL, 
