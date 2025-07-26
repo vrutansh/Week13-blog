850157b36f2019b86cdb4153ba4aad188dc6ebeb
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-07-26 19:00:16 +0530

    signin signup

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,15 @@
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
 export const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    return <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signup" />
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
+            </div>
+        </div>
+    </div>
 }
-
